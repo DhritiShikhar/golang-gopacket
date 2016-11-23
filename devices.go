d82d3aee7caf633bc76100957db9c46d50db0363
Dhriti Shikhar <dhrish20@gmail.com>
2016-11-23 15:00:03 +0530

    Adds program which lists all the devices in current machine

---
+++ devices.go
@@ -0,0 +1,27 @@
+// This program prints all devices on your machine
+
+// Author: Dhriti Shikhar
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	// enumerate all network interfaces on current machine
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Interface structure has name, description and address fields
+	for _, d := range devices {
+		fmt.Println("\n", d.Name)
+		fmt.Println("\n", d.Description)
+		fmt.Println("-----------------------")
+	}
+}
